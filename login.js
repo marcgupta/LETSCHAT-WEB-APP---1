5a2d6881985ae59cb063eb78f4566993525b4f0a
marcgupta <81474423+marcgupta@users.noreply.github.com>
2022-06-05 23:10:44 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+
+function addUser() {
+
+    user_name = document.getElementById("username").value
+    localStorage.setItem("user_name" , user_name)
+
+    window.location = "letschat_room.html"
+    
+}
